70a360851d7170692e7ba7b472c1ea73cb0a6a71
kazuhisa <kazuhisa@36cd9982-b127-0410-854c-bdec8c54013d>
2007-02-18 18:18:32 +0000

    * set IPW_RX_BUF_SIZE 2048  to avoid overflow in rx.

--- trunk/iwi2200/defines.h
+++ trunk/iwi2200/defines.h
@@ -936,7 +936,8 @@ typedef unsigned char UInt8;
 #define IP_CH(ip)	((u_char *)ip)
 #define IP_LIST(ip)	IP_CH(ip)[2],IP_CH(ip)[3],IP_CH(ip)[4],IP_CH(ip)[5]
 
-#define IPW_RX_BUF_SIZE (3000)
+//#define IPW_RX_BUF_SIZE (3000)
+#define IPW_RX_BUF_SIZE (2048)
 
 #define memcpy_toio(a,b,c)	memcpy((void *)(a),(b),(c))
 
