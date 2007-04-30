4ba9138e488a6ff65f10e053b10afb3ee21fbeb0
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2007-04-30 23:00:41 +0000

    ipw_bg_alive_start()

--- iwi3945/defines.h
+++ iwi3945/defines.h
@@ -985,6 +985,16 @@ typedef unsigned char UInt8;
 #define ETH_P_AARP	0x80F3		/* Appletalk AARP		*/
 #define ETH_P_IPX	0x8137		/* IPX over DIX			*/
 
+#define IW_QUAL_QUAL_UPDATED    0x01    /* Value was updated since last read */
+#define IW_QUAL_LEVEL_UPDATED   0x02
+#define IW_QUAL_NOISE_UPDATED   0x04
+#define IW_QUAL_ALL_UPDATED     0x07
+#define IW_QUAL_QUAL_INVALID    0x10    /* Driver doesn't provide value */
+#define IW_QUAL_LEVEL_INVALID   0x20
+#define IW_QUAL_NOISE_INVALID   0x40
+#define IW_QUAL_ALL_INVALID     0x70
+#define IW_QUAL_DBM             0x08    /* Level + Noise are dBm */
+#define CHAN_UTIL_RATE_LCM 95040
 
 
 
