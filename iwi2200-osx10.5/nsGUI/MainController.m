d27c4842e5e9b74efd51590af42e6fc28865715d
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2008-02-06 16:46:42 +0000

    iwi2200 for mac os x 10.5

---
+++ iwi2200-osx10.5/nsGUI/MainController.m
@@ -0,0 +1,33 @@
+#import "MainController.h"
+
+@implementation MainController
+
+- (IBAction)Cancel:(id)sender
+{
+}
+
+- (IBAction)Connect:(id)sender
+{
+}
+
+- (IBAction)LEDAction:(id)sender
+{
+}
+
+- (IBAction)ModeACtion:(id)sender
+{
+}
+
+- (IBAction)NetworkAction:(id)sender
+{
+}
+
+- (IBAction)PowerAction:(id)sender
+{
+}
+
+- (IBAction)RefreshAction:(id)sender
+{
+}
+
+@end
