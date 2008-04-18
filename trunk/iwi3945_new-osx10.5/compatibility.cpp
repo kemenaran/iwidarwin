6858e768cfa62387d67c446a3a2994427d69ef07
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2008-04-18 16:05:44 +0000

    scan

--- trunk/iwi3945_new-osx10.5/compatibility.cpp
+++ trunk/iwi3945_new-osx10.5/compatibility.cpp
@@ -7790,8 +7790,8 @@ int ieee80211_open(struct ieee80211_local *local)
 	
 	IOLog("1st scan\n");
 	if (res==0)
-	//iwl_scan((struct iwl3945_priv*)get_my_priv());
-	ieee80211_sta_start_scan(dev, NULL, 0);
+	iwl_scan((struct iwl3945_priv*)get_my_priv());
+	//ieee80211_sta_start_scan(dev, NULL, 0);
 	else
 	IOLog(" not ready for 1st scan\n");
 	
