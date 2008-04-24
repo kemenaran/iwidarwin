4d045eca868888b61cea84d5af05563d4d4d150a
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2008-04-24 22:00:23 +0000

    bug

--- iwi4965-osx10.5/compatibility.cpp
+++ iwi4965-osx10.5/compatibility.cpp
@@ -839,7 +839,7 @@ IM_HERE_NOW();
 	/*if (timer->expires>0)
 	timei=jiffies_to_msecs(timer->expires);
 	else timei=5000;*/
-	timei=timer->expires;
+	timei=timer->expires*10;
 	clock_interval_to_deadline(timei,kMillisecondScale,&deadline);
 	IOLog("timer->expires %d timei %d deadline %d\n",timer->expires,timei,deadline);
 	thread_call_enter1_delayed(timer_func[timer->vv],(void*)timer->data,deadline);
