705b0cfad1d03be31b2e3f7af56fba5860379ba6
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2008-04-25 19:09:34 +0000

    networks

--- iwi4965-osx10.5/iwi4965.cpp
+++ iwi4965-osx10.5/iwi4965.cpp
@@ -150,16 +150,17 @@ int configureConnection(kern_ctl_ref ctlref, u_int unit, void *userdata, int opt
 		}	
 	}
 	if(opt == 2){
-		IOLog("Request scan disabled\n");
+		IOLog("Request scan\n");
 		//iwl_scan((struct iwl4965_priv*)get_my_priv());
-		/*struct ieee80211_local *local=hw_to_local(get_my_hw());
+		struct ieee80211_local *local=hw_to_local(get_my_hw());
 		if (local)
 		{
 			struct net_device *dev=local->mdev;
 			ieee80211_sta_start_scan(dev, NULL, 0);
+			//ieee80211_sta_start_scan(dev, (u8*)"<hidden>", sizeof("<hidden>"));
 		}
 		else
-			IOLog("not ready to scan \n");*/
+			IOLog("not ready to scan \n");
 	}
 
 	return(0);
