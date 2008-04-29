90307620d8a6918a85e6dff372968e590f701817
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2008-04-29 22:23:59 +0000

    outputPacket2

--- trunk/iwi3945_new-osx10.5/compatibility.h
+++ trunk/iwi3945_new-osx10.5/compatibility.h
@@ -13,6 +13,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif		
+	extern int __ieee80211_tx(struct ieee80211_local *local, struct sk_buff *skb,
+			  struct ieee80211_txrx_data *tx);
 	extern int ieee80211_sta_req_scan(struct net_device *dev, u8 *ssid, size_t ssid_len);
 	extern void ieee80211_sta_scan_work(struct work_struct *work);
 	extern int ieee80211_open(struct ieee80211_local *local);
