d5612a67b744978262e9bf6f40f47229fb48394e
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2008-05-02 18:25:29 +0000

    scan

--- trunk/iwi4965-osx10.5/iwl4965-base.c
+++ trunk/iwi4965-osx10.5/iwl4965-base.c
@@ -6696,16 +6696,16 @@ static void iwl4965_alive_start(struct iwl4965_priv *priv)
 
 	IWL_DEBUG_INFO("ALIVE processing complete.\n");
 	
-	struct ieee80211_local *local=hw_to_local(priv->hw);
-	struct net_device *dev=local->mdev;
+	//struct ieee80211_local *local=hw_to_local(priv->hw);
+	//struct net_device *dev=local->mdev;
 	
 	
 	wake_up_interruptible(&priv->wait_command_queue);
 
 	if (priv->error_recovering)
 		iwl4965_error_recovery(priv);
-	else
-	ieee80211_sta_req_scan(dev,NULL,0);
+	//else
+	//ieee80211_sta_req_scan(dev,NULL,0);
 	
 	return;
 
