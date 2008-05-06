6ae7021eca0f9dbd85bdc209654597dc829fd667
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2008-05-06 10:24:57 +0000

    scan

--- iwi3945_new-osx10.5/compatibility.cpp
+++ iwi3945_new-osx10.5/compatibility.cpp
@@ -3300,9 +3300,9 @@ IM_HERE_NOW();
 	ieee80211_rx_bss_info(dev, mgmt, len, rx_status, 0);
 	struct ieee80211_sub_if_data *sdata = (ieee80211_sub_if_data*)IEEE80211_DEV_TO_SUB_IF(dev);
 	struct ieee80211_if_sta *ifsta = &sdata->u.sta;
-	if (ifsta->state != IEEE80211_AUTHENTICATE &&
+	/*if (ifsta->state != IEEE80211_AUTHENTICATE &&
 	    ifsta->state != IEEE80211_ASSOCIATE &&
-	    ifsta->state != IEEE80211_ASSOCIATED)
+	    ifsta->state != IEEE80211_ASSOCIATED)*/
 	ieee80211_authenticate(dev,ifsta);//hack
 	
 }
