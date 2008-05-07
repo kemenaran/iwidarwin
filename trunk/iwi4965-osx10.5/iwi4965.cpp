feb99eec8f306987e7d822a13b7ac744a7ddcd2a
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2008-05-07 11:11:18 +0000

    scan

--- trunk/iwi4965-osx10.5/iwi4965.cpp
+++ trunk/iwi4965-osx10.5/iwi4965.cpp
@@ -403,10 +403,11 @@ void darwin_iwi4965::check_firstup(void)
 		setProperty(kIOMACAddress, my_mac_addr, kIOEthernetAddressSize);
 	}
 	//queue_te2(1,OSMemberFunctionCast(thread_call_func_t,this,&darwin_iwi4965::adapter_start),NULL,NULL,true);
-	/*struct ieee80211_local *local=hw_to_local(get_my_hw());
-	int r=ieee80211_open(local);
+	struct ieee80211_local *local=hw_to_local(get_my_hw());
+	//int r=ieee80211_open(local);
 	struct net_device *dev=local->mdev;
-	if (!r) ieee80211_sta_req_scan(dev,NULL,0);*/
+	//if (!r) 
+	ieee80211_sta_req_scan(dev,NULL,0);
 	//ieee80211_open(local);
 }
 
