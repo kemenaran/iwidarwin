bd5ff481ba7a8f77b7c706a618dc822b163eb4bf
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2008-05-12 20:11:53 +0000

    sts

--- trunk/iwi3945_new-osx10.5/iwl3945-base.c
+++ trunk/iwi3945_new-osx10.5/iwl3945-base.c
@@ -7436,7 +7436,7 @@ static int iwl3945_mac_config_interface(struct ieee80211_hw *hw, int if_id,
 	if (priv->interface_id != if_id) {
 		IWL_DEBUG_MAC80211("leave - interface_id != if_id\n");
 		mutex_unlock(&priv->mutex);
-		//return 0;
+		return 0;
 	}
 
 	if (priv->iw_mode == IEEE80211_IF_TYPE_AP) {
@@ -7479,10 +7479,12 @@ static int iwl3945_mac_config_interface(struct ieee80211_hw *hw, int if_id,
 		else {
 			rc = iwl3945_commit_rxon(priv);
 			if ((priv->iw_mode == IEEE80211_IF_TYPE_STA) && !rc)
+				//iwl3945_add_station(priv,
+				//	priv->active_rxon.bssid_addr, 1, 0);
 				iwl3945_add_station(priv,
-					priv->active_rxon.bssid_addr, 1, 0);
-			
-		/*	struct ieee80211_local *local = hw_to_local(hw);
+					priv->active_rxon.bssid_addr, 0, 0);
+		
+			/*struct ieee80211_local *local = hw_to_local(hw);
 			struct ieee80211_sub_if_data *sdata=NULL;
 			list_for_each_entry(sdata, &local->sub_if_list, list) 
 			{
@@ -7491,20 +7493,19 @@ static int iwl3945_mac_config_interface(struct ieee80211_hw *hw, int if_id,
 				if (sta_id==0) break;
 				sdata=NULL;
 			}		
-			//struct net_device *dev = local->mdev;
+			struct net_device *dev = local->scan_dev;
 			if (sdata)
 			{
 				struct ieee80211_if_sta *ifsta = &sdata->u.sta;
-				if (!ifsta->associated)
-				{
+				//if (!ifsta->associated)
+				//{
 				IOLog("hacking authenticate stabssid=" MAC_FMT "\n", MAC_ARG(sdata->u.sta.bssid));
-
-				//ieee80211_sta_config_auth(sdata->dev, ifsta);
+				ieee80211_sta_config_auth(dev, ifsta);
 			//	ieee80211_authenticate(sdata->dev,ifsta);
 				//ieee80211_associate(dev,ifsta);
 				//sdata->type = IEEE80211_IF_TYPE_STA;
 				//ifsta->state=IEEE80211_AUTHENTICATE;
-				}
+				//}
 			}*/
 		}
 
