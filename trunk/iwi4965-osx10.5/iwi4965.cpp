53c1d9a693ae21272f7fcdc79ab2474f48a6665d
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2008-05-12 14:51:20 +0000

    sd

--- trunk/iwi4965-osx10.5/iwi4965.cpp
+++ trunk/iwi4965-osx10.5/iwi4965.cpp
@@ -408,7 +408,7 @@ void darwin_iwi4965::check_firstup(void)
 	//queue_te2(1,OSMemberFunctionCast(thread_call_func_t,this,&darwin_iwi4965::adapter_start),NULL,NULL,true);
 	struct ieee80211_local *local=hw_to_local(get_my_hw());
 	//int r=ieee80211_open(local);
-	struct net_device *dev=local->mdev;
+	struct net_device *dev=local->scan_dev;
 	if (dev) 
 	ieee80211_sta_req_scan(dev,NULL,0);
 	//ieee80211_open(local);
