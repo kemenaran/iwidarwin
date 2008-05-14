7ea0372865053d523fefe6828e0efb6f0d10bc9c
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2008-05-14 12:54:19 +0000

    scan

--- trunk/iwi4965-osx10.5/iwi4965.cpp
+++ trunk/iwi4965-osx10.5/iwi4965.cpp
@@ -150,8 +150,8 @@ int configureConnection(kern_ctl_ref ctlref, u_int unit, void *userdata, int opt
 		}	
 	}
 	if(opt == 2){
-		IOLog("Request scan - disabled!\n");
-		/*struct ieee80211_local *local=hw_to_local(get_my_hw());
+		IOLog("Request scan!\n");
+		struct ieee80211_local *local=hw_to_local(get_my_hw());
 		if (local)
 		{
 			struct net_device *dev=local->scan_dev;
@@ -161,7 +161,7 @@ int configureConnection(kern_ctl_ref ctlref, u_int unit, void *userdata, int opt
 			IOLog("not ready to scan \n");
 		}
 		else
-			IOLog("not ready to scan \n");*/
+			IOLog("not ready to scan \n");
 	}
 
 	return(0);
@@ -406,11 +406,11 @@ void darwin_iwi4965::check_firstup(void)
 		setProperty(kIOMACAddress, my_mac_addr, kIOEthernetAddressSize);
 	}
 	//queue_te2(1,OSMemberFunctionCast(thread_call_func_t,this,&darwin_iwi4965::adapter_start),NULL,NULL,true);
-	struct ieee80211_local *local=hw_to_local(get_my_hw());
+	/*struct ieee80211_local *local=hw_to_local(get_my_hw());
 	//int r=ieee80211_open(local);
 	struct net_device *dev=local->scan_dev;
 	if (dev) 
-	ieee80211_sta_req_scan(dev,NULL,0);
+	ieee80211_sta_req_scan(dev,NULL,0);*/
 	//ieee80211_open(local);
 }
 
