a1fb3ef434c6735f7f4d9973b62f1fcf28ed8c8f
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2008-05-15 08:05:53 +0000

    scan

--- iwi3945_new-osx10.5/compatibility.cpp
+++ iwi3945_new-osx10.5/compatibility.cpp
@@ -3431,12 +3431,12 @@ IM_HERE_NOW();
 	       dev->name, beacon ? "Beacon" : "Probe Response",
 	       MAC_ARG(mgmt->sa), MAC_ARG(mgmt->da));
 #endif
-if (!beacon)
+/*if (!beacon)
 	{
 		IOLog("hacking add station\n");
 		struct ieee80211_if_sta *ifsta = &sdata->u.sta;
 		bcopy(mgmt->sa,sdata->u.sta.bssid,ETH_ALEN);
-	}
+	}*/
 	
 	baselen = (u8 *) mgmt->u.beacon.variable - (u8 *) mgmt;
 	if (baselen > len)
@@ -6953,7 +6953,7 @@ IM_HERE_NOW();
 					    ssid, ssid_len);
 		if (!rc) {
 			local->sta_scanning = 1;
-			//local->scan_dev = dev;
+			local->scan_dev = dev;
 		}
 		return rc;
 	}
