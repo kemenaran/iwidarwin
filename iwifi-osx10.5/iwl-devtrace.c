bb03d00eac5b8be96a186367b06b4b32d5b1ab80
jalavoui <jalavoui@36cd9982-b127-0410-854c-bdec8c54013d>
2009-10-13 20:34:43 +0000

    netif

--- iwifi-osx10.5/iwl-devtrace.c
+++ iwifi-osx10.5/iwl-devtrace.c
@@ -5,9 +5,9 @@
 #define CREATE_TRACE_POINTS
 #include "iwl-devtrace.h"
 
-EXPORT_TRACEPOINT_SYMBOL(iwlwifi_dev_ioread32);
-EXPORT_TRACEPOINT_SYMBOL(iwlwifi_dev_iowrite32);
-EXPORT_TRACEPOINT_SYMBOL(iwlwifi_dev_rx);
-EXPORT_TRACEPOINT_SYMBOL(iwlwifi_dev_ucode_event);
-EXPORT_TRACEPOINT_SYMBOL(iwlwifi_dev_ucode_error);
+//EXPORT_TRACEPOINT_SYMBOL(iwlwifi_dev_ioread32);
+//EXPORT_TRACEPOINT_SYMBOL(iwlwifi_dev_iowrite32);
+//EXPORT_TRACEPOINT_SYMBOL(iwlwifi_dev_rx);
+//EXPORT_TRACEPOINT_SYMBOL(iwlwifi_dev_ucode_event);
+//EXPORT_TRACEPOINT_SYMBOL(iwlwifi_dev_ucode_error);
 #endif
